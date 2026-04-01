2c73eace5af3cb862a9cc8c56fbcd3ad906d5212
Malhar <84181301+malhar-patil@users.noreply.github.com>
2026-03-31 18:06:40 -0700

    Add: subtree-of-a-binary-tree - submission-10

---
+++ Data Structures & Algorithms/subtree-of-a-binary-tree/submission-10.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public: 
+    bool isSame(TreeNode*& root, TreeNode*& subRoot){
+        if((root == NULL && subRoot!=NULL) || (root!=NULL && subRoot==NULL)){
+            return false;
+        }
+        if(root==NULL && subRoot==NULL){
+            return true;
+        }
+
+        if(root->val != subRoot->val){
+            return false;
+        }
+        bool left = isSame(root->left, subRoot->left);
+        bool right = isSame(root->right, subRoot->right);
+        cout<<left<<" "<<right<<endl;
+        if(left && right){
+            return true;
+        }
+        return false;
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot){
+        if(root == NULL){
+            return false;
+        }
+        
+        if(root->val == subRoot->val){
+            cout<<root->val<<" "<<subRoot->val<<endl;
+            if(isSame(root, subRoot)){
+                return true;
+            }
+        }
+        bool left = isSubtree(root->left, subRoot);
+        bool right = isSubtree(root->right, subRoot);
+        if(left || right){
+            return true;
+        }
+        return false;
+    }
+};
