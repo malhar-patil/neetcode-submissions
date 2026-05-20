ce8f94c58ca13b1ed38032f07b995ea356e77966
Malhar <84181301+malhar-patil@users.noreply.github.com>
2026-05-20 16:32:07 -0700

    Add: last-stone-weight - submission-6

---
+++ Data Structures & Algorithms/last-stone-weight/submission-6.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> heap;
+        for(int i=0;i<stones.size();i++){
+            heap.push(stones[i]);
+        }
+
+        while(heap.size() > 1){
+            int a = heap.top(); heap.pop();
+            int b = heap.top(); heap.pop();
+
+            if(a == b){
+                continue;
+            }
+            else{
+                heap.push(a-b);
+            }
+        }
+        if(heap.size() == 0){return 0;}
+        return heap.top();
+    }
+};
