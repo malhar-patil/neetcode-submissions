71eccffb6ba2578218a26786ca9bdf616462e284
Malhar <84181301+malhar-patil@users.noreply.github.com>
2026-06-08 15:02:04 -0700

    Add: jump-game-ii - submission-3

---
+++ Data Structures & Algorithms/jump-game-ii/submission-3.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int minJumps = 0;
+        int index = 0;
+
+        while(index < nums.size()-1){
+            int i = index+1;
+            int maxDistance = 0;
+            int maxIndex = 0;
+
+            cout<<i<<" "<<index + nums[index]<<endl;
+            while(i < nums.size() && i <= (index + nums[index])){
+                
+                if(i == nums.size()-1){
+                    return minJumps+1;
+                }
+
+                if(i + nums[i] >= maxDistance){
+                    maxDistance = nums[i] + i;
+                    maxIndex = i;
+                }
+                i++;                
+            }
+            minJumps++;
+            index = maxIndex;
+            cout<<index<<endl;
+        }
+        return minJumps;
+    }
+};
