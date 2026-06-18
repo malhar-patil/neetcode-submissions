3e8c2aa0116a3acc763d68bc7233b62a6c162cea
Malhar <84181301+malhar-patil@users.noreply.github.com>
2026-06-18 16:03:33 -0700

    Add: longest-increasing-subsequence - submission-6

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-6.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    map<pair<int,int>, int> memo;
+
+    int dfs(int index, int num, vector<int>& nums){
+        if(index >= nums.size()){
+            return 0;
+        }
+        if(memo.find({index,num}) != memo.end()){
+            return memo[{index,num}];
+        }
+
+        int len = 0;
+        for(int i=index;i<nums.size();i++){
+            if(nums[i] > num){
+                len = max(len, dfs(i+1, nums[i], nums)+1);
+            }
+        }
+        memo[{index,num}] = len;
+        return len;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int res = 1;
+        for(int i=0;i<nums.size();i++){
+            int num = nums[i];
+            res = max(res,dfs(i+1, num, nums)+1);
+        }
+        return res;
+    }
+};
