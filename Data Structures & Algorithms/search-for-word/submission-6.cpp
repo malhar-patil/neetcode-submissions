97f9547771f2c6015f6d868965aff7ee19c1661c
Malhar <84181301+malhar-patil@users.noreply.github.com>
2026-07-04 15:48:05 -0700

    Add: search-for-word - submission-6

---
+++ Data Structures & Algorithms/search-for-word/submission-6.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool res = false;
+    void dfs(int i, int j, vector<vector<char>>& board, string word, string temp,vector<vector<int>>& visited){
+        if( (i>=0 && i < board.size()) && (j >=0 && j < board[0].size()) && temp.length() > word.length()){
+            return;
+        }
+        if(temp + board[i][j]== word){
+            res = true;
+            return;
+        }
+
+        visited[i][j] = 1;
+
+        //up
+        if( (i-1>=0 && i-1 < board.size()) && (j >=0 && j < board[0].size())&& visited[i-1][j] == -1){
+            dfs(i-1, j, board, word, temp+board[i][j], visited);
+        }
+        //down
+        if( (i+1>=0 && i+1 < board.size()) && (j >=0 && j < board[0].size())&& visited[i+1][j] == -1){
+            dfs(i+1, j, board, word, temp+board[i][j], visited);
+        }
+        //left
+        if( (i>=0 && i < board.size()) && (j-1 >=0 && j-1 < board[0].size())&& visited[i][j-1] == -1){
+            dfs(i, j-1, board, word, temp+board[i][j], visited);
+        }
+        //right
+        if( (i>=0 && i < board.size()) && (j+1 >=0 && j+1 < board[0].size())&& visited[i][j+1] == -1){
+            dfs(i, j+1, board, word, temp+board[i][j], visited);
+        }
+
+        visited[i][j] = -1;
+        return;
+    }
+    bool exist(vector<vector<char>>& board, string word) {
+        vector<vector<int>> visited(board.size(), vector<int>(board[0].size(), -1));
+        string temp = "";
+        for(int i=0;i<board.size();i++){
+            for(int j=0;j<board[0].size();j++){
+                if(board[i][j] == word[0]){
+                    dfs(i,j, board, word, temp, visited);
+                }
+            }
+        }
+        return res;
+    }
+};
