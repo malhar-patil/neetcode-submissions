31b92bc940c03bda95cdb467e318e34c29d5bb46
Malhar <84181301+malhar-patil@users.noreply.github.com>
2026-07-12 16:32:25 -0700

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l = 0;
+        int profit = 0;
+
+        for(int r=0;r<prices.size();r++){
+            profit = max(profit, prices[r]-prices[l]);
+            if(prices[r] < prices[l]){
+                l = r;
+            }
+        }
+        return profit;
+    }
+};
