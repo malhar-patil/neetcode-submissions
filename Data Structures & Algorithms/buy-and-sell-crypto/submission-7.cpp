8dd8fbe7d2c2543be13015bc0970603fff8fed9f
Lowbyte <84181301+malhar-patil@users.noreply.github.com>
2026-09-15 06:24:19 -0700

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l=0;
+        int ans = 0;
+        for(int r=1;r<prices.size();r++){
+            ans = max(ans, prices[r] - prices[l]);
+            if(prices[r] < prices[l]){
+                l = r;
+            }
+        }
+        return ans;
+    }
+};
